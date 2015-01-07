6dd934adc4c92415bdc4bae17970fd19d2314f63
gohlivor <gohlivor@gmail.com>
2015-01-07 10:09:49 -0600

    changes made at codemash
    -some ovr stuff
    -load level scripts

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+
+            int current = Application.loadedLevel;
+            current++;
+
+            if (current >= 4)
+                current = 0;
+
+            Application.LoadLevel(current);
+        
+        }
+
+        if (Input.GetButtonDown("Cancel"))
+            Application.Quit();
+
+	}
+}
